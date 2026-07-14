936072df66f891ad92df5c87ef200425ca9a391f
Rohit <mohiter852@gmail.com>
2026-07-14 14:59:36 +0530

    live backend url from render

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api/expenses";
+const API_BASE_URL = `${import.meta.env.VITE_API_URL || "http://localhost:5000/api/expenses"}/api/expenses`
 
 export const getExpenses = () => axios.get(API_BASE_URL);
 
